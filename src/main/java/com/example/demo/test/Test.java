d3b9c4cbafa11ccb86b2bb40f09bc14b5d48c417
Jared <jaredjaskolski@yahoo.com>
2023-03-29 15:38:05 -0400

    init

---
+++ src/main/java/com/example/demo/test/Test.java
@@ -0,0 +1,13 @@
+package com.example.demo.test;
+
+public class Test {
+	public String name;
+	
+	public String getName() {
+		return this.name;
+	}
+	
+	public void setName(String name) {
+		this.name = name;
+	}
+}
